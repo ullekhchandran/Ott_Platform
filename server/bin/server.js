c4aab30ee7f5fe4534ed937d1b0b5a00c81fe348
ullekhchandran <ullekhchandran@gmail.com>
2025-03-23 03:11:25 +0530

    modified

--- server/bin/server.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const app = express();
-
-const PORT = process.env.PORT || 3000; // Render requires process.env.PORT
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
