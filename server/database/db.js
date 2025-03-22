a84ffbca5f76cd9670edb9ff655c8ce1b85b2787
ullekhchandran <ullekhchandran@gmail.com>
2025-03-22 20:26:28 +0530

    forth commit

--- server/database/db.js
+++ server/database/db.js
@@ -1,7 +1,7 @@
 const mongoose= require('mongoose');
 require('dotenv').config()
 
-mongoose.connect('mongodb+srv://ullekhchandran:Threedots%4099@cluster0.lddmr.mongodb.net/')
+mongoose.connect(process.env.MONGODB_ATLAS_CONNECTION_URL )
 
 const db= mongoose.connection;
 db.on('error',console.error.bind(console,'MongoDB connection error:'));
