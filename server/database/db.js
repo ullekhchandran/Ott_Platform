1530559f0d07ab3a3c93e5128207f637c1a83bf2
ullekhchandran <ullekhchandran@gmail.com>
2025-03-22 20:15:53 +0530

    third commit

--- server/database/db.js
+++ server/database/db.js
@@ -1,7 +1,7 @@
 const mongoose= require('mongoose');
 require('dotenv').config()
 
-mongoose.connect('mongodb+srv://ullekhchandran:Threedots@99@cluster0.lddmr.mongodb.net/')
+mongoose.connect('mongodb+srv://ullekhchandran:Threedots%4099@cluster0.lddmr.mongodb.net/')
 
 const db= mongoose.connection;
 db.on('error',console.error.bind(console,'MongoDB connection error:'));
