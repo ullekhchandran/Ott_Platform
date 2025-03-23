43691cf8deb298dcb09d8ba611a78c8178540d74
ullekhchandran <ullekhchandran@gmail.com>
2025-03-23 12:11:25 +0530

    updated

--- server/app.js
+++ server/app.js
@@ -4,7 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var session = require('express-session');
-const cors =require('cors')
+const cors = require('cors');
 require('dotenv').config();
 
 var indexRouter = require('./routes/index');
